08602af1da3cddcae544ed8ce56491df68f612f3
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-06-18 14:33:18 +0300

    Add files via upload

--- 04. references/references.cpp
+++ 04. references/references.cpp
@@ -8,8 +8,8 @@ int main() {
 
    int &c = a;
 
-   std::cout << " References:  " << a << std::endl;
-   std::cout << " References:  " << b << std::endl;
+   std::cout << " References:  " << a << std::endl; // Значение переменной а
+   std::cout << " References:  " << b << std::endl; 
    std::cout << " References:  " << &c << std::endl;
 
    system("pause");
