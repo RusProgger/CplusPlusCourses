c3e8024f1e7bd13be3483a216eb2337c55c28f13
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-06-19 16:38:35 +0300

    Add files via upload

---
+++ 05. arrays/array.cpp
@@ -0,0 +1,44 @@
+/* 
+
+    .size() — возвращает размер
+
+    .at(i) — доступ с проверкой границ
+
+    .front() / .back() — первый/последний элемент
+
+    .fill(value) — заполнить массив значением
+
+    .begin() / .end() — итераторы
+
+
+*/
+
+
+
+#include <iostream>
+
+
+int main() {
+    
+    int numb[] = {1, 5, 10, 55, 64, 38};
+
+    std::cout << numb[1] << std::endl;
+
+
+
+    std::string user[5];
+
+    user[0] = "Alex";
+    user[1] = "Anjeli";
+    user[2] = "Ivan";
+    user[3] = "Tolik";
+    user[4] = "Julia";
+
+    std::cout << "User = " << user[4] << std::endl;
+
+    int res = sizeof(user) / sizeof(user[0]);
+
+    std::cout << "Width array: " << res << std::endl;
+
+    return 0;
+}
