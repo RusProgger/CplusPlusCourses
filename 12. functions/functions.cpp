d7c2ce9261d1e3d451c4d52eb9f2583f57a9d9ee
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-07-03 12:13:56 +0300

    Add files via upload

---
+++ 12. functions/functions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void textHello() {
+    std::cout << "Hello, me function" << std::endl;
+}
+
+void textHello_1() {
+    std::cout << "Function - 1" << std::endl;
+}
+
+void textHello_2() {
+    std::cout << "Function - 2" << std::endl;
+}
+
+int main() {
+
+    int x = 10;
+
+    textHello();
+
+    textHello_1();
+
+    textHello_2();
+ 
+    
+}
