3357bbe67fe39b617c70ef53d4dd7311b3c32ff3
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-07-11 14:30:58 +0300

    Add files via upload

---
+++ 13. call by value/funbyvalue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void funN(int a) {
+   ++a;
+   std::cout << "Fuction funN: " << a;
+}
+
+int main() {
+    int a = 1;
+    std::cout << "A: " << a << std::endl;
+    
+    funN(a);
+}
