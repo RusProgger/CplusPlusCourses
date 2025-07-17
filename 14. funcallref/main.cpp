c77363d7b6a5b339af9f17bcc7b7258c0a76d48b
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2025-07-17 13:33:41 +0300

    Add files via upload

---
+++ 14. funcallref/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void funP(int *p) {
+    *p = *p + 1;
+    std::cout << "FunP: " << *p << "\n";
+}
+
+void funR(int &ref) {
+    ++ref;
+    std::cout << "funR: " << ref;
+}
+
+int main() {
+    int a = 10;
+    funP(&a);
+
+    int res = 100;
+    funR(res);
+}
